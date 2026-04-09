860ef8a12d1c741ebce8347fe44e3c11028207f7
LuisMenchero <luismencheroperezcejuela@gmail.com>
2026-04-09 21:01:47 +0200

    organizacion archivos iniciales

--- NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
+++ NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.ninisvstheworld;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        StackPane root = new StackPane();
+        ImageView fondoPaner = new ImageView("Animaciones/Ninis/AdripanCargado.gif");
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 320, 240);
+        scene.getStylesheets().add(this.getClass().getResource("./EstiloPrueba.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
