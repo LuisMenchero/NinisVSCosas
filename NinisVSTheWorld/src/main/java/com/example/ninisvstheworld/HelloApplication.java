0421364b06e2937c916966d2b5b1e1f995d32016
LuisMenchero <luismencheroperezcejuela@gmail.com>
2026-04-10 00:00:08 +0200

    recolocacion de lineas de la scene principal

--- NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
+++ NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.ninisvstheworld;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,7 +16,6 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         ImageView fondoPaner = new ImageView();
         fondoPaner.imageProperty().set(bgImage);
         StackPane root = new StackPane();
@@ -23,9 +23,13 @@ public class HelloApplication extends Application {
         fondoPaner.fitWidthProperty().bind(root.widthProperty());
         fondoPaner.fitWidthProperty().bind(root.widthProperty());
         fondoPaner.setPreserveRatio(true);
-        Scene scene = new Scene(root, 1020, 480);
+        Scene scene = new Scene(root, 1280, 720);
         stage.setTitle("Ninis vs TheWorld - Luis & Diego");
         stage.setScene(scene);
         stage.show();
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
     }
 }
