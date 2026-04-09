a45b6a89e6e65c59e75c6fe78c0a2ecc7fe4ca94
LuisMenchero <luismencheroperezcejuela@gmail.com>
2026-04-09 22:10:44 +0200

    retoques de archivos e implementacion de fondo

--- NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
+++ NinisVSTheWorld/src/main/java/com/example/ninisvstheworld/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.ninisvstheworld;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -10,15 +11,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private final Image bgImage = new Image("file:src/main/resources/Animaciones/Items/FondoNvsW.png");
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        ImageView fondoPaner = new ImageView();
+        fondoPaner.imageProperty().set(bgImage);
         StackPane root = new StackPane();
-        ImageView fondoPaner = new ImageView("Animaciones/Ninis/AdripanCargado.gif");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 320, 240);
-        scene.getStylesheets().add(this.getClass().getResource("./EstiloPrueba.css").toExternalForm());
-        stage.setTitle("Hello!");
+        root.getChildren().add(fondoPaner);
+        fondoPaner.fitWidthProperty().bind(root.widthProperty());
+        fondoPaner.fitWidthProperty().bind(root.widthProperty());
+        fondoPaner.setPreserveRatio(true);
+        Scene scene = new Scene(root, 1020, 480);
+        stage.setTitle("Ninis vs TheWorld - Luis & Diego");
         stage.setScene(scene);
         stage.show();
     }
