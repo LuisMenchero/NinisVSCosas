adc164b5c14de11bb61de2103a2c413ba6d87726
LuisMenchero <luismencheroperezcejuela@gmail.com>
2026-04-14 19:23:14 +0200

    retoques de todas las plantas y clase Guevara casi terminado

--- src/main/java/modelos/Adripan.java
+++ src/main/java/modelos/Adripan.java
@@ -2,13 +2,40 @@ package modelos;
 
 import javafx.scene.layout.Pane;
 
+import javax.swing.text.html.ImageView;
+
 public class Adripan extends Nini {
     // --- ATRIBUTOS EXTRA ---
-
+    private boolean estaCargado = false;
     // --- CONSTRUCTOR ---
     public Adripan(double columna, double fila, Pane root) {
-        super(columna, fila, 80, 25, 0, "Animaciones/Ninis/AdripanEsperando.gif", root);
+        super(columna, fila, 80, 25, 10, "Animaciones/Ninis/AdripanEsperando.gif", root);
     }
 
-    // --- MÉTODOS ---
+    //--- MÉTODOS ---
+//    @Override
+//    public void actualizar(double tiempoFrames, ArrayList<Cosa> cosas) {
+//
+//    }
+//
+//    @Override
+//    public void disparar(ArrayList<Cosa> cosas) {
+//    }
+
+//    public void explotar(double tiempoFrames) {
+//        if (estaCargado && -----) {
+//
+//        }
+//    }
+
+    public boolean cargar(double tiempoFrames) {
+        if (tiempoFrames <= cooldownDisparo && !estaCargado) {
+            setImagenNiniImage("Animaciones/Ninis/AdripanRecarga.gif");
+            estaCargado = true;
+        }
+        if (estaCargado) {
+            setImagenNiniImage("Animaciones/Ninis/AdripanCargado.gif");
+        }
+        return false;
+    }
 }
--- src/main/java/modelos/Guevara.java
+++ src/main/java/modelos/Guevara.java
@@ -6,9 +6,28 @@ public class Guevara extends Nini {
     // --- ATRIBUTOS EXTRA ---
 
     // --- CONSTRUCTOR ---
-    public Guevara(double columna, double fila, Pane root) {
+    public Guevara(double columna, double fila, Pane root, Celda[][] terreno) {
         super(columna, fila, 500, 0, 0, "Animaciones/Ninis/Guevara_Idle.gif", root);
+        potenciarNinis(terreno);
     }
 
     // --- MÉTODOS ---
+    //    @Override
+//    public void actualizar(double tiempoFrames, ArrayList<Cosa> cosas) {
+//
+//    }
+//
+//    @Override
+//    public void disparar(ArrayList<Cosa> cosas) {
+//    }
+
+    public void potenciarNinis(Celda[][] terreno) {
+        for (int i = 0; i < Cuadricula.columnas; i++) {
+            for (int j = 0; j < Cuadricula.filas; j++) {
+                if (terreno[i][j].getNini() != null) {
+                    terreno[i][j].getNini().setPotenciado(true);
+                }
+            }
+        }
+    }
 }
