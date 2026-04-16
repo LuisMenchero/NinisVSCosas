efb7f132ae43f9e76cccee6d34b64381182e8674
diiegosk <diegoss2502@gmail.com>
2026-04-16 20:12:25 +0200

    Gestor de Inventario y escena seleccionar

--- src/main/java/escenas/EscenaSeleccionar.java
+++ src/main/java/escenas/EscenaSeleccionar.java
@@ -1,6 +1,7 @@
 package escenas;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -12,7 +13,15 @@ public class EscenaSeleccionar {
         fondo.setFitWidth(1280);
         fondo.setFitHeight(720);
 
-        Pane root = new Pane(fondo);
+        Button btnSalir = new Button("Salir");
+        btnSalir.setLayoutX(1200);
+        btnSalir.setLayoutY(10);
+        btnSalir.setOnAction(evento -> {
+            EscenaMenu escenaMenu = new EscenaMenu();
+            stage.setScene(escenaMenu.construir(stage));
+        });
+
+        Pane root = new Pane(fondo, btnSalir);
         return new Scene(root, 1280, 720);
     }
 
